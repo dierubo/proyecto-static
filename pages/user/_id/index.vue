cffda4debd507468f9bcb87c0b3dd260e2edece9
drubiom <drubiom@minsait.com>
2019-03-11 15:42:23 +0100

    first commit

---
+++ pages/user/_id/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>{{ user.id }}</div>
+    <div>{{ user.name }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  asyncData (context) {
+    console.log(context.route.params);
+    return axios.get(`https://jsonplaceholder.typicode.com/users/${context.route.params.id}`)
+      .then(res => {
+        console.log(res);
+        return {
+          user: res.data
+        }
+      })
+  },
+  created () {
+    return axios.get(`https://jsonplaceholder.typicode.com/users/`)
+      .then(res => {
+        console.log(res);
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Página de usuarios</h3>
+
+    <nuxt-link to="/user/1">User 1</nuxt-link>
+    <nuxt-link to="/user/2">User 2</nuxt-link>
+    <nuxt-link to="/user/3">User 3</nuxt-link>
+    <nuxt-link to="/user/4">User 4</nuxt-link>
+    <nuxt-link to="/user/5">User 5</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
